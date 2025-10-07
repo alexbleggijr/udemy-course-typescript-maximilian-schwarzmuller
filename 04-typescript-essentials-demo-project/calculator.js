8fc311d7a8c0a3c381cd8d93aed0c58ab7510f67
alexbleggijr <alexbleggijr@gmail.com>
2025-10-07 14:30:00 -0300

    compilation and execution

---
+++ 04-typescript-essentials-demo-project/calculator.js
@@ -0,0 +1,51 @@
+function calculateInvestment(data) {
+    var initialAmount = data.initialAmount, annualContribution = data.annualContribution, expectedReturn = data.expectedReturn, duration = data.duration;
+    if (initialAmount < 0) {
+        return "Initial investment amount must be at least zero.";
+    }
+    if (duration <= 0) {
+        return "No valid amount of years provided.";
+    }
+    if (expectedReturn <= 0) {
+        return "Expected return must be at least zero.";
+    }
+    var total = initialAmount;
+    var totalContributions = 0;
+    var totalInterestEarned = 0;
+    var annualResults = [];
+    for (var index = 0; index < duration; index++) {
+        total = total * (1 + expectedReturn);
+        totalInterestEarned = total - totalContributions - initialAmount;
+        totalContributions = totalContributions + annualContribution;
+        total = total + annualContribution;
+        annualResults.push({
+            year: "Year ".concat(index + 1),
+            totalAmount: total,
+            totalInterestEarned: totalInterestEarned,
+            totalContributions: totalContributions,
+        });
+    }
+    return annualResults;
+}
+function printResults(results) {
+    if (typeof results === "string") {
+        console.log();
+        return;
+    }
+    for (var _i = 0, results_1 = results; _i < results_1.length; _i++) {
+        var yearEndResult = results_1[_i];
+        console.log(yearEndResult.year);
+        console.log("Total: ".concat(yearEndResult.totalAmount.toFixed(0)));
+        console.log("Total Contributions: ".concat(yearEndResult.totalContributions.toFixed(0)));
+        console.log("Total Interest Earned: ".concat(yearEndResult.totalInterestEarned.toFixed(0)));
+        console.log("----------");
+    }
+}
+var investmentData = {
+    initialAmount: 5000,
+    annualContribution: 500,
+    expectedReturn: 0.08,
+    duration: 10,
+};
+var results = calculateInvestment(investmentData);
+printResults(results);
