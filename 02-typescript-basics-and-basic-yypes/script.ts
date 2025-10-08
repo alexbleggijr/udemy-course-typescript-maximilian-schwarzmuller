12713e5535961e8b39b871b1213a4cec9e1ebc0d
alexbleggijr <alexbleggijr@gmail.com>
2025-10-08 14:30:00 -0300

    optional values and typescript

--- 02-typescript-basics-and-basic-yypes/script.ts
+++ 02-typescript-basics-and-basic-yypes/script.ts
@@ -1,1 +1,11 @@
-function process(val: unknown) {}
+function generateError(msg: string) {
+  throw new Error(msg);
+}
+
+generateError("");
+
+type User = {
+  name: string;
+  age: number;
+  role?: "admin" | "guest";
+};
