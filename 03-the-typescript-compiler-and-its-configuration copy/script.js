594336e2de803987dc52202287a5e00a1b8b446b
alexbleggijr <alexbleggijr@gmail.com>
2025-10-08 14:30:00 -0300

    module introduction

--- 03-the-typescript-compiler-and-its-configuration copy/script.js
+++
@@ -1,6 +0,0 @@
-var input = "";
-var didProvideImput = input || false;
-console.log(didProvideImput);
-var input2 = "";
-var didProvideImput2 = input !== null && input !== void 0 ? input : false;
-console.log(didProvideImput2);
