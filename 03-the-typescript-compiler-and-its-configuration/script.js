e08d09d6f405bd090fe834792507e2b529d7a0a7
alexbleggijr <alexbleggijr@gmail.com>
2025-10-08 14:30:00 -0300

    typescript project setup and creating a tsconfig.json file

---
+++ 03-the-typescript-compiler-and-its-configuration/script.js
@@ -0,0 +1,6 @@
+var input = "";
+var didProvideImput = input || false;
+console.log(didProvideImput);
+var input2 = "";
+var didProvideImput2 = input !== null && input !== void 0 ? input : false;
+console.log(didProvideImput2);
