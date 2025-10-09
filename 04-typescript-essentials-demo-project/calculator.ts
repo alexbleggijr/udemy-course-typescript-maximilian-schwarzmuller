67442c7cc86c9c47c547165f2b73e1475d236482
alexbleggijr <alexbleggijr@gmail.com>
2025-10-09 14:30:00 -0300

    getting started and first steps

---
+++ 04-typescript-essentials-demo-project/calculator.ts
@@ -0,0 +1,12 @@
+// data:
+// initial amount
+// annual contribution
+// expected return
+// duration
+
+function calculateInvestment(data) {};
+
+function printResults(results) {};
+
+const results = calculateInvestment() {};
+printResults(results);
