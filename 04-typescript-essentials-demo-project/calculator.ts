1e04627327fef5c6e5eed7102cbfdda0a13a4bc1
alexbleggijr <alexbleggijr@gmail.com>
2025-10-10 14:30:00 -0300

    adding application logic

--- 04-typescript-essentials-demo-project/calculator.ts
+++ 04-typescript-essentials-demo-project/calculator.ts
@@ -15,7 +15,41 @@ type InvestmentResult = {
 type calculationResult = InvestmentResult[] | string;
 
 function calculateInvestment(data: InvestmentData): calculationResult {
+    const {initialAmount, annualContribution, expectedReturn, duration} = data;
 
+    if (initialAmount < 0) {
+        return 'Initial investment amount must be at least zero.';
+    }
+
+    if (duration <= 0) {
+        return 'No valid amount of years provided.';
+    }
+
+    if (expectedReturn <= 0) {
+        return 'Expected return must be at least zero.';
+    }
+
+    let total = initialAmount;
+    let totalContributions = 0;
+    let totalInterestEarned = 0;
+
+    const annualResults: InvestmentResult[] = [];
+
+    for (let index = 0; index < duration; index++) {
+        total = total * (1 + expectedReturn);
+        totalInterestEarned = total - totalContributions - initialAmount;
+        totalContributions = totalContributions + annualContribution;
+        total = total + annualContribution;
+
+        annualResults.push({
+            year: `Year ${index + 1}`,
+            totalAmount: total,
+            totalInterestEarned,
+            totalContributions
+        });
+    }
+
+    return annualResults;
 };
 
 function printResults(results) {};
